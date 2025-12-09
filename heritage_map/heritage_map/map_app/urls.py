2979e67c357cd3d9cf96676677ba066212fe3006
Oleksii Pshenychnuy <oleksijpsenicnij01@gmail.com>
2025-12-09 12:52:51 +0200

    ---

--- heritage_map/heritage_map/map_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views # Імпортуємо всі функції із views.py
-
-urlpatterns = [
-    # Визначаємо, що кореневий маршрут додатку ('') веде на функцію views.index
-    path('', views.index, name='index'), 
-]
