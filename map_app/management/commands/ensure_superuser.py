1a891b9369aacfb6697f3f8b3207bed3cface8a7
Oleksii Pshenychnuy <oleksijpsenicnij01@gmail.com>
2026-01-12 19:53:13 +0200

    .

--- map_app/management/commands/ensure_superuser.py
+++ map_app/management/commands/ensure_superuser.py
@@ -2,13 +2,14 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from decouple import config
 
+
 class Command(BaseCommand):
     help = "Create superuser if it doesn't exist (for Render deploy)"
 
     def handle(self, *args, **options):
-        username = config('DJANGO_SUPERUSER_USERNAME', default=None)
-        email = config('DJANGO_SUPERUSER_EMAIL', default='')
-        password = config('DJANGO_SUPERUSER_PASSWORD', default=None)
+        username = config("DJANGO_SUPERUSER_USERNAME", default=None)
+        email = config("DJANGO_SUPERUSER_EMAIL", default="")
+        password = config("DJANGO_SUPERUSER_PASSWORD", default=None)
 
         if not username or not password:
             self.stdout.write("Superuser env vars not set, skipping.")
